7a6fa73bcf2b19afa3af3132584efcd6fc39bbfa
zGabrielR1 <77293588+zGabrielR1@users.noreply.github.com>
2021-03-02 22:09:38 -0300

    Update arquivo001.py
    Apenas adicionei alguns comentários no primeiro exercício da primeira aula.

--- aula-20210112/arquivo001.py
+++ aula-20210112/arquivo001.py
@@ -1,4 +1,5 @@
-
+# Aqui colocamos uma varíavel <valor>
+# Na qual será usada para representar outros métodos
 valor = 1
 print(__name__)
 if __name__ == '__main__':
@@ -9,7 +10,7 @@ if __name__ == '__main__':
     numero1 = 500
     numero2 = 34.1247
     numero3 = 13j
-
+    
     print(numero1, numero2, numero3)
     print(numero1 / numero2 * numero3)
     print(numero1 / numero2 + (numero2 * 5))
@@ -17,10 +18,12 @@ if __name__ == '__main__':
     mensagem = "Curso \'{}\', {}"
 
     # Método de substituição
+    # Substitui a primeira string (Java), pela segunda (Novembro)
     print(mensagem.format("Java", "Novembro"))
     print(mensagem.format("Golang", "Fevereiro"))
 
     # Usando f-strings
+    # A f-string "chama a string da variável
     java = "Java"
     php = "php"
 
@@ -51,21 +54,21 @@ if __name__ == '__main__':
 
     # Operadores de comparação
     # >, <, >=, <=, ==, !=
-    print(2 > 5)
-    print(10 > 8)
-    print(100 != 100)
-    print(50 >= 50)
+    print(2 > 5) # Falso
+    print(10 > 8) # Verdadeiro
+    print(100 != 100) # Falso
+    print(50 >= 50) # Verdadeiro
 
     # Operadores aritméticos
-    print(10 % 3)
-    print(10 ** 2)
-    print(10 // 3)
+    print(10 % 3) # Função modular
+    print(10 ** 2) # Potenciação
+    print(10 // 3) # Resto da divisão
 
 
     # Operadores de atribuição
     numero1 = numero1 + 10
     numero1 -= 200 # numero1 =  numero1 - 200
-    numero1 *= 2
+    numero1 *= 2 # numero1 = numero1 * 2 (numero1 vezes 2) 
     print(numero1)
 
     # Operadores lógicos
@@ -73,27 +76,27 @@ if __name__ == '__main__':
 
     # and
     # and retorna True se e somente se os 2 lados da expressão forem verdadeiros
-    expressao = 10 > 2 and 5 < 7
+    expressao = 10 > 2 and 5 < 7 # Verdadeiro
     print(expressao)
-    expressao = 1 > 3 and 4 < 9
+    expressao = 1 > 3 and 4 < 9 # Falso
     print(expressao)
 
     # or
     # or retorna True se ao menos 1 dos lados da expressão for verdadeiro
-    expressao = 100 < 3 or 2 > 0
+    expressao = 100 < 3 or 2 > 0 # Verdadeiro
     print(expressao)
 
     # not
     # not nega um valor (ou expressão)
-    expressao = not 100 > 10
+    expressao = not 100 > 10 # Falso
     print(expressao)
-    expressao = not 10 > 1000
+    expressao = not 10 > 1000 # Verdadeiro
     print(expressao)
 
     # Operadores de pertencimento
     # in, not in
     texto = "The book is on the table."
 
-    print("banana" in texto)
-    print("table" in texto)
-    print("carne" not in texto)
+    print("banana" in texto) # Operador booleano falso
+    print("table" in texto) # Verdadeiro
+    print("carne" not in texto) # Verdadeiro
